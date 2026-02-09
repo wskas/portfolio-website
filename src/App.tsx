20142a122ae847ce3143959988b369ca09c5947a
Winson Sutanto Kasman <winsonsutantokasman@gmail.com>
2026-02-10 00:23:03 +0800

    add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,40 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import Hero from './components/Hero';
+import ContactMe from './components/ContactMe';
+import Projects from './components/Projects';
+import Footer from './components/footer';
+import Header from './components/Header';
+import About from './components/About';
+import WorkExperiences from './components/WorkExperiences';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="fixed inset-0 bg-white">
+        <div className="absolute inset-0 bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:24px_24px]"/>
+        <div className="absolute inset-0">
+          <div className="absolute top-0 right-0 w-96 h-96 bg-white rounded-full mix-blend-soft-light filter blur-[60px] opacity-30">
+            </div>
+            <div className="absolute top-0 left-0 md:w-96 md:h-96 w-72 h-72 bg-white rounded-full mix-blend-soft-light filter blur-[60px] opacity-30">
+              </div><div className="absolute bottom-0 left-0 w-96 h-96 bg-white rounded-full mix-blend-soft-light filter blur-[60px] opacity-30">
+                </div>
+                 <div className="absolute bottom-0 right-0 w-96 h-96 bg-white rounded-full mix-blend-soft-light filter blur-[60px] opacity-20">
+                  </div>
+                  </div>
+                  </div>
+      <div className='main'>
+          <div className='gradient' />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className='content relative'>
+        <Header />
+        <Hero />
+        <About />
+        <Projects />
+        <WorkExperiences />
+        {/* <ContactMe /> */}
+        <Footer />
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
 }
